7ceded48af280dc35e87ea51bdc4865edb904ef1
Niyati Bansal <niyatibansal14@gmail.com>
2026-04-28 21:37:37 +0530

    updated API URL to deploy backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE = "http://127.0.0.1:8000";
+const BASE = "https://logistic-mitra.onrender.com";
 
 export const predictRoute = async (data) => {
   const res = await axios.post(`${BASE}/predict`, data);
